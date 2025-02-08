a5049df10856d36366bed25241ff82f42a307eff
choidy180 <choidy180@naver.com>
2025-02-08 12:43:35 +0900

    'update-project'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,11 +8,6 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            {/* <style>
-                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""></link>
-                <link href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Smooch+Sans:wght@100..900&display=swap" rel="stylesheet"></link>
-            </style> */}
             <body
               className={``}
             >
